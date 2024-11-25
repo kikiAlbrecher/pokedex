60d0aafa0e926cd497bbfc17b32dc65e28ae34c4
kikiAlbrecher <christina.albrecher@gmx.at>
2024-11-25 10:58:30 +0100

    Update

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
-let pokemonIndex = 0;
 let currentPokemons = [];
 const BASE_URL = "https://pokeapi.co/api/v2/";
-const LIMIT_URL = "pokemon?limit=";
-const LIMIT = 2;
+const POKE_URL = "pokemon";
+const EVOLUTION_URL = "evolution-chain";
+const LIMIT_URL = "?limit=";
+const LIMIT = 10;
 let OFFSET = 0;
+let evolutionChains = [];
+let i = 0;
+let allfilteredPokemons = [];
+const MAX_RESULTS = 3;
+let currentDisplayedIndex = MAX_RESULTS;
 
 function init() {
   getPokemons();
+  getEvoChains();
 }
 
 async function getPokemons() {
   showLoadingSpinner(true);
-
   try {
-    let response = await fetch(BASE_URL + LIMIT_URL + LIMIT + "&offset=" + OFFSET);
-    let responseToJson = await response.json();
-    console.log(responseToJson);
-
+    const responseToJson = await fetchPokemonsData();
     for (let index = 0; index < responseToJson.results.length; index++) {
-      let pokeName = responseToJson.results[index].name;
-      const pokeUrl = responseToJson.results[index].url;
-      let detailResponse = await fetch(pokeUrl);
-      let pokeDetails = await detailResponse.json();
-      let pokeImg = pokeDetails.sprites.other.dream_world.front_default;
-      let pokeType = pokeDetails.types.map(typeInfo => typeInfo.type.name).join(' ');
-      let pokeId = pokeDetails.id
-      pushPokeToArr(pokeName, pokeUrl, pokeId, pokeImg, pokeType);
+      const pokeDetails = await fetchPokemonDetails(responseToJson.results[index].url);
+      processPokemonDetails(pokeDetails);
     }
-    console.log(currentPokemons);
     renderPokemons();
   } catch (e) {
-    let errorMessageRef = document.getElementById('error_message');
-    document.getElementById('error_message').classList.remove('d_none');
-    errorMessageRef.innerHTML = getErrorMessage();
-    currentPokemons = [];
+    handleError(e);
   } finally {
     showLoadingSpinner(false);
+  }
+}
+
+async function fetchPokemonsData() {
+  let response = await fetch(BASE_URL + POKE_URL + LIMIT_URL + LIMIT + "&offset=" + OFFSET);
+  let responseToJson = await response.json();
+  return responseToJson;
+}
+
+async function fetchPokemonDetails(url) {
+  let detailResponse = await fetch(url);
+  let pokeDetails = await detailResponse.json();
+  return pokeDetails;
+}
+
+function processPokemonDetails(pokeDetails) {
+  let pokeName = pokeDetails.name;
+  let pokeId = pokeDetails.id;
+  let pokeImg = pokeDetails.sprites.other.dream_world.front_default;
+  let pokeType = pokeDetails.types.map(typeInfo => typeInfo.type.name).join(' ');
+  let pokeHeight = pokeDetails.height;
+  let pokeWeight = pokeDetails.weight;
+  let baseExperience = pokeDetails.base_experience;
+  let abilities = pokeDetails.abilities.map(ableTo => ableTo.ability.name).join(', ');
+  let moves = pokeDetails.moves.slice(0, 3).map(firstMoves => firstMoves.move.name).join(', ');
+  let pokeStats = pokeDetails.stats;
+
+  pushPokeToArr(pokeName, pokeDetails.url, pokeId, pokeImg, pokeType, pokeHeight, pokeWeight, baseExperience, abilities, moves, pokeStats);
+}
+
+function handleError(e) {
+  let errorMessageRef = document.getElementById('error_message');
+  document.getElementById('error_message').classList.remove('d_none');
+  errorMessageRef.innerHTML = getErrorMessage(e);
+  currentPokemons = [];
+  document.getElementById('button_regulation').classList.add('d_none');
+}
+
+async function getEvoChains() {
+  try {
+    let response = await fetch(BASE_URL + EVOLUTION_URL + LIMIT_URL + LIMIT + "&offset=" + OFFSET);
+    let responseToJson2 = await response.json();
+    for (let index = 0; index < responseToJson2.results.length; index++) {
+      const evoUrl = responseToJson2.results[index].url;
+      let evoResponse = await fetch(evoUrl);
+      let evoDetails = await evoResponse.json();
+      let evoChainStarter = evoDetails.chain.species.name;
+      let evoTo1 = evoDetails.chain.evolves_to.map(evo1 => evo1.species.name).join(' ');
+      let evoTo2 = evoDetails.chain.evolves_to.map(evo2 => evo2.evolves_to.map(evo2details => evo2details.species.name).join(' ')).join(' ');
+      pushEvoToArr(evoChainStarter, evoTo1, evoTo2);
+    }
+  } catch (e) {
+    return ``;
   }
 }
 
@@ -50,50 +96,213 @@ function showLoadingSpinner(show) {
   }
 }
 
-function pushPokeToArr(pokeName, pokeUrl, pokeId, pokeImg, pokeType) {
-  currentPokemons.push({ pokeName, pokeUrl, pokeId, pokeImg, pokeType })
+function pushPokeToArr(pokeName, pokeUrl, pokeId, pokeImg, pokeType, pokeHeight, pokeWeight, baseExperience, abilities, moves, pokeStats) {
+  currentPokemons.push({ pokeName, pokeUrl, pokeId, pokeImg, pokeType, pokeHeight, pokeWeight, baseExperience, abilities, moves, pokeStats });
+}
+
+function pushEvoToArr(evoChainStarter, evoTo1, evoTo2) {
+  evolutionChains.push({ evoChainStarter, evoTo1, evoTo2 });
 }
 
 function renderPokemons() {
   let pokemonRef = document.getElementById('pokemon');
   pokemonRef.innerHTML = "";
 
-  currentPokemons.forEach((pokemon) => {
-    pokemonRef.innerHTML += getPokemonCardsTemplate(pokemon);
+  currentPokemons.forEach((pokemon, i) => {
+    pokemonRef.innerHTML += getPokemonCardsTemplate(pokemon, i);
+  });
+  document.getElementById('load_more_btn').classList.remove('d_none');
+}
+
+function searchPokemon() {
+  let searchTerm = getSearchTerm();
+
+  if (searchTerm.length >= 3) {
+    toggleLoadMoreButton(true);
+    let filteredPokemons = filterPokemonsBySearchTerm(searchTerm);
+    renderFilteredPokemons(filteredPokemons);
+  } else {
+    renderPokemons(currentPokemons);
+    toggleLoadMoreButton(false);
+  }
+}
+
+function getSearchTerm() {
+  return window.innerWidth > 820 ? document.getElementById('search_input_desktop').value : document.getElementById('search_input_mobile').value;
+}
+
+function filterPokemonsBySearchTerm(searchTerm) {
+  let searchTermLow = searchTerm.toLowerCase();
+  let filteredPokemons = [];
+
+  currentPokemons.forEach(pokemon => {
+    if (pokemon.pokeName.toLowerCase().includes(searchTermLow) &&
+      !filteredPokemons.find(existingPokemon => existingPokemon.pokeName === pokemon.pokeName)) {
+      filteredPokemons.push(pokemon);
+    }
+  });
+  return filteredPokemons;
+}
+
+function renderFilteredPokemons(filteredPokemons) {
+  allfilteredPokemons = filteredPokemons;
+  currentDisplayedIndex = MAX_RESULTS;
+
+  let filteredPokemonContainer = document.getElementById('pokemon');
+  filteredPokemonContainer.innerHTML = '';
+
+  if (filteredPokemons.length === 0) {
+    filteredPokemonContainer.innerHTML = getNoFoundTemplate();
+  } else {
+    displayPokemons(filteredPokemons.slice(0, MAX_RESULTS), filteredPokemonContainer);
+    if (filteredPokemons.length > MAX_RESULTS) {
+      showMoreResults(filteredPokemonContainer);
+    }
+  }
+}
+
+function displayPokemons(pokemons, container) {
+  pokemons.forEach((pokemon, i) => {
+    container.innerHTML += getPokemonCardsTemplate(pokemon, i);
   });
 }
 
-function openOverlay() {
+function showMoreResults(container) {
+  const moreResultsRef = getMoreResultsTemplate();
+  container.innerHTML += moreResultsRef;
+}
 
+function showMoreFilteredPokemons() {
+  const nextPokemons = allfilteredPokemons.slice(currentDisplayedIndex, currentDisplayedIndex + MAX_RESULTS);
+  displayPokemons(nextPokemons, document.getElementById('pokemon'));
+  currentDisplayedIndex += MAX_RESULTS;
+
+  if (currentDisplayedIndex >= allfilteredPokemons.length) {
+    const loadMoreResultsButton = document.getElementById('show_more_search_results');
+    loadMoreResultsButton.classList.add('d_none');
+    document.getElementById('more_results_infotext').classList.add('d_none');
+  }
+}
+
+function toggleGreyOverlay(i) {
+  let greyOverlayRef = document.getElementById('grey_overlay');
+  greyOverlayRef.classList.toggle('d_none');
+  renderOverlay(i);
+}
+
+function renderOverlay(i) {
+  let pokemonOverlayRef = document.getElementById('pokemon_overlay');
+  pokemonOverlayRef.innerHTML = '';
+
+  // Wenn es gefilterte Pokémon gibt
+  if (allfilteredPokemons.length > 0) {
+    // Hole das Pokémon aus allfilteredPokemons anhand des Index
+    const selectedPokemon = allfilteredPokemons[i];
+
+    let currentPokemonIndex = currentPokemons.findIndex(pokemon => pokemon.pokeId === selectedPokemon.pokeId);
+    console.log(currentPokemonIndex);
+
+    // Wenn das Pokémon in currentPokemons gefunden wird
+    if (currentPokemonIndex !== -1) {
+      i = currentPokemonIndex;
+      // Rendern das Overlay mit dem richtigen Pokémon basierend auf currentPokemonIndex
+      pokemonOverlayRef.innerHTML = getPokemonOverlay(i);
+
+      // Spezifische UI-Anpassungen
+      document.getElementById('previous_poke').classList.add('d_none');
+      document.getElementById('next_poke').classList.add('d_none');
+      document.getElementById('sequence_attributes').classList.remove('space-between_center');
+      document.getElementById('sequence_attributes').classList.add('center_center');
+    }
+  } else {
+    // Falls keine gefilterten Ergebnisse, direkt das Overlay mit dem Index i anzeigen
+    pokemonOverlayRef.innerHTML = getPokemonOverlay(i);
+  }
+}
+
+function addOverlay(event) {
+  let overlayRef = document.getElementById('pokemon_overlay');
+  overlayRef.classList.remove('d_none');
+  event.stopPropagation();
+}
+
+function removeOverlay(event) {
+  let greyOverlayRef = document.getElementById('grey_overlay');
+  greyOverlayRef.classList.add('d_none');
+  event.stopPropagation();
+}
+
+function switchTab(event, tabName) {
+  const tabs = document.querySelectorAll('.tab_button');
+  tabs.forEach(tab => tab.classList.remove('active'));
+
+  const panels = document.querySelectorAll('.tab_panel');
+  panels.forEach(panel => panel.classList.remove('active'));
+
+  event.currentTarget.classList.add('active');
+  document.getElementById(tabName).classList.add('active');
+}
+
+function onePokeBack(i) {
+  let x = (i - 1);
+  if (i > 0) {
+    renderOverlay(x);
+  } else {
+    x = currentPokemons.length - 1;
+    renderOverlay(x);
+  }
+}
+
+function onePokeForward(i) {
+  let y = (i + 1);
+  if (i < currentPokemons.length - 1) {
+    renderOverlay(y);
+  } else {
+    y = 0;
+    renderOverlay(y);
+  }
+}
+
+function getPokemonImage(pokeName) {
+  let pokemon = currentPokemons.find(poke => normalizePokemonName(poke.pokeName) === normalizePokemonName(pokeName));
+  return pokemon ? pokemon.pokeImg : ``;
+}
+
+function normalizePokemonName(pokeName) {
+  if (pokeName) {
+    return pokeName.split(' ')[0].toLowerCase();
+  }
+  return ``;
 }
 
 function loadMorePokes() {
   showLoadingSpinner(true);
   OFFSET += LIMIT;
   getPokemons();
+  getEvoChains();
 }
 
+function toggleLoadMoreButton(show) {
+  const loadMoreButton = document.getElementById('load_more_btn');
+  show ? loadMoreButton.classList.add('d_none') : loadMoreButton.classList.remove('d_none');
+}
 
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
 
-// function searchPokemon() {
-//   let searchTerm = document.getElementById('searchId').value;
-//   if (searchTerm.length >= 3) {
-//     console.log('Suchbegriff: ', searchTerm);
-//     let searchTermLow = searchTerm.toLowerCase();
-//     console.log('Suchbegriff: ', searchTermLow);
-//     allPokemons.filter(pokemon => {
-//       if (pokemon.name.toLowerCase().includes(searchTermLow)) {
-//         searchArray.push(pokemon);
-//       }
-//     });
-//     console.log('gesuchte Pokemon: ', searchArray);
-//     let pokeContainer = document.getElementById('PokeId');
-//     pokeContainer.innerHTML = '';
-//     for (let i = 0; i < searchArray.length; i++) {
-//       let singlePokemon = searchArray[i];
-//       pokeContainer.innerHTML += `
-//                 <div>${singlePokemon['name']}</div>
-//         `;
-//     }
-//   }
-// }
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let backToTopRef = document.getElementById('back_to_top');
+  if (window.scrollY > 20) {
+    backToTopRef.classList.remove('d_none');
+  } else {
+    backToTopRef.classList.add('d_none');
+  }
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,21 +1,215 @@
 function getErrorMessage() {
     return `
-        <h3>Ein Problem ist aufgetreten. Bitte lade die Seite neu.</h3>
+        <h4>A problem occured. Please reload the website.</4>
     `;
 }
 
-function getPokemonCardsTemplate(pokemon) {
+function getMoreResultsTemplate() {
     return `
-                    <div class="pokemon_card display_flex_column" onclick="openOverlay()">
+        <div class="more_search_results display_flex_column center_center" id="more_search_results">
+            <p class="more_results_info" id="more_results_infotext">
+                Only ${MAX_RESULTS} pokémons are shown. Further results are available.
+            </p>
+            <button class="btn" id="show_more_search_results" onclick="showMoreFilteredPokemons()">
+                show more results
+            </button>
+        </div>
+    `;
+  }
+
+function getPokemonCardsTemplate(pokemon, i) {
+    let typeCollection = pokemon.pokeType.split(' ');
+    let typeImages = typeCollection.map(type => {
+        return `
+            <img class="pokemon_type ${type}" src="assets/icons/${type}.svg" alt="${type}">
+        `;
+    }).join('');
+
+    return `
+                    <div class="pokemon_card display_flex_column" onclick="toggleGreyOverlay(${i})">
                         <div class="poke_id_name display_flex_row space-between_center">
-                            <h5>#${pokemon.pokeId}</h5>
-                            <h5>${pokemon.pokeName}</h5>
+                            <h4>#${pokemon.pokeId}</h4>
+                            <h4>${pokemon.pokeName}</h4>
                         </div>
-                        <img class="pokemon_img" src="${pokemon.pokeImg}" alt="${pokemon.pokeName}" />
+                        <div class="pokemon_img_area ${typeCollection[0]} display_flex_row center_center">
+                        <img class="pokemon_img" src="${pokemon.pokeImg}" alt="${pokemon.pokeName}">
+                        </div>
                         <div class="pokemon_attributes display_flex_row center_center">
-                            <img class="pokemon_type btn_small">${pokemon.pokeType}</img>
+                            ${typeImages}
                         </div>
                     </div>
     `;
 }
 
+function getNoFoundTemplate() {
+    return `
+        <div>No Pokémon found.</div>
+    `;
+}
+
+function getPokemonOverlay(i) {
+    let typeCollection = currentPokemons[i].pokeType.split(' ');
+    let typeImages = typeCollection.map(type => {
+        return `
+            <img class="pokemon_type ${type}" src="assets/icons/${type}.svg" alt="${type}">
+        `;
+    }).join('');
+    let move = currentPokemons[i].moves.split(' ');
+    let evolution = evolutionChains.find(chain =>
+        normalizePokemonName(chain.evoChainStarter) === normalizePokemonName(currentPokemons[i].pokeName) ||
+        normalizePokemonName(chain.evoTo1) === normalizePokemonName(currentPokemons[i].pokeName) ||
+        normalizePokemonName(chain.evoTo2) === normalizePokemonName(currentPokemons[i].pokeName)
+    );
+
+    return `
+        <div class="overlay_content display_flex_column">
+            <div class="overlay_headline display_flex_row space-between_center">
+                <h2>#${currentPokemons[i].pokeId}</h2>
+                <h2>${currentPokemons[i].pokeName}</h2>
+                <button class="btn_small" onclick="removeOverlay(event)">
+                    <img src="assets/icons/close.png" title="close" alt="close">
+                </button>
+            </div>
+            <img class="pokemon_img_in_overlay ${typeCollection[0]}" src="${currentPokemons[i].pokeImg}" alt="${currentPokemons[i].pokeName}">
+            <div class="type_with_change_btn display_flex_row space-between_center" id="sequence_attributes">
+                <button class="btn_small" id="previous_poke" onclick="onePokeBack(${[i]})">
+                    <img src="assets/icons/backward.png" title="previous" alt="back">
+                </button>
+                <div class="pokemon_attributes display_flex_row center_center">
+                    ${typeImages}
+                </div>
+                <button class="btn_small" id="next_poke" onclick="onePokeForward(${[i]})">
+                    <img src="assets/icons/forward.png" title="next" alt="forward">
+                </button>
+            </div>
+
+            <div class="tabs_container">
+                <div class="tabs_header display_flex_row space-around_center">
+                    <button class="tab_button active" onclick="switchTab(event, 'main')"><h3>main</h3></button>
+                    <button class="tab_button" onclick="switchTab(event, 'stats')"><h3>stats</h3></button>
+                    <button class="tab_button" onclick="switchTab(event, 'evo')"><h3>evo chain</h3></button>
+                </div>
+        
+                <div class="tab_content">
+                    <div id="main" class="tab_panel ">
+                        <h3 class="h3_variant">Main Info</h3>
+                        <div class="display_flex_column">
+                            <div class="main_infos display_flex_row">
+                                <h5 class="headline_width">Height: </h5><h5>${currentPokemons[i].pokeHeight} m</h5>
+                            </div>
+                            <div class="main_infos display_flex_row">
+                                <h5 class="headline_width">Weight: </h5><h5>${currentPokemons[i].pokeWeight} kg</h5>
+                            </div>
+                            <div class="main_infos display_flex_row">
+                                <h5 class="headline_width">Base Experience: </h5><h5>${currentPokemons[i].baseExperience}</h5>
+                            </div>
+                            <div class="main_infos display_flex_row">
+                                <h5 class="headline_width">Abilities: </h5><h5>${currentPokemons[i].abilities}</h5>
+                            </div>
+                            <div class="main_infos display_flex_row">
+                                <h5 class="headline_width">3 Moves: </h5>
+                                <ul class="moves_list">
+                                    <li><h5>${move[0]}</h5></li>
+                                    <li><h5>${move[1]}</h5></li>
+                                    <li><h5>${move[2]}</h5></li> 
+                                </ul>
+                            </div>
+                        </div>
+                    </div>    
+                    <div id="stats" class="tab_panel active">
+                        <h3 class="h3_variant">Stats</h3>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">HP: ${currentPokemons[i].pokeStats[0].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[0].base_stat / 250) * 100}%;"></div>
+                                </div>
+                                <span>250</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Attack: ${currentPokemons[i].pokeStats[1].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[1].base_stat / 134) * 100}%;"></div>
+                                </div>
+                                <span>134</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Defense: ${currentPokemons[i].pokeStats[2].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[2].base_stat / 180) * 100}%;"></div>
+                                </div>
+                                <span>180</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Special attack: ${currentPokemons[i].pokeStats[3].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[3].base_stat / 154) * 100}%;"></div>
+                                </div>
+                                <span>154</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Special defense: ${currentPokemons[i].pokeStats[4].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[4].base_stat / 125) * 100}%;"></div>
+                                </div>
+                                <span>125</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Speed: ${currentPokemons[i].pokeStats[5].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${(currentPokemons[i].pokeStats[5].base_stat / 150) * 100}%;"></div>
+                                </div>
+                                <span>150</span>
+                            </div>
+                        </div>
+                        <div class="stats_values display_flex_row">
+                            <h5 class="headline_width">Base value: ${currentPokemons[i].pokeStats[0].base_stat + currentPokemons[i].pokeStats[1].base_stat + currentPokemons[i].pokeStats[2].base_stat + currentPokemons[i].pokeStats[3].base_stat + currentPokemons[i].pokeStats[4].base_stat + currentPokemons[i].pokeStats[5].base_stat}</h5>
+                            <div class="stats_representation display_flex_row center_center">
+                                <span>0</span>
+                                <div class="progress_bar_container">
+                                    <div class="progress_bar" style="width: ${((currentPokemons[i].pokeStats[0].base_stat + currentPokemons[i].pokeStats[1].base_stat + currentPokemons[i].pokeStats[2].base_stat + currentPokemons[i].pokeStats[3].base_stat + currentPokemons[i].pokeStats[4].base_stat + currentPokemons[i].pokeStats[5].base_stat) / 993) * 100}%;"></div>
+                                </div>
+                                <span>993</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="evo" class="tab_panel">
+                        <h3 class="h3_variant">Evo Chain</h3>
+                        <div class="display_flex_column">
+                            <div class="evo_chain_images display_flex_row space-around_center">
+                ${evolution ? `
+                    ${evolution.evoChainStarter ? `<img class="evo_img" src="${getPokemonImage(evolution.evoChainStarter)}" alt="${evolution.evoChainStarter}">` : ``}
+                    ${evolution.evoTo1 ? `<p>>></p><img class="evo_img" src="${getPokemonImage(evolution.evoTo1)}" alt="${evolution.evoTo1}">` : ``}
+                    ${evolution.evoTo2 ? `<p>>></p><img class="evo_img" src="${getPokemonImage(evolution.evoTo2)}" alt="${evolution.evoTo2}">` : ``}
+                ` : 'No evolution available'}
+            </div>
+            <div class="display_flex_row space-around_center">
+                ${evolution ? `
+                    ${evolution.evoChainStarter ? `<p>${evolution.evoChainStarter}</p>` : ``}
+                    ${evolution.evoTo1 ? `<p>${evolution.evoTo1}</p>` : ``}
+                    ${evolution.evoTo2 ? `<p>${evolution.evoTo2}</p>` : ``}
+                ` : 'No evolution available'}
+            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
